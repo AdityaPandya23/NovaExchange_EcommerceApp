b0b46c57a17d5bf1140e5726f0972af178f842e0
Aditya Pandya <pandyaaditya600@gmail.com>
2022-09-16 23:52:04 +0530

    Fetch Products

--- README.md
+++ README.md
@@ -11,3 +11,5 @@
 2).Slug: You use a slug when you want to refer to an item while preserving the ability to see, at a glance, what the item is i.e human readable identifier.
 3). To import press ctrl + " " and tab.
 4).we can use Link instead of a to get rid of page refreshing everytime happens when we click.
+
+8)Axios Library is used to fetch data from Backend
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,23 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
+// import data from '../data';
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+    // fetchData to commit.
+  });
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.Slug}>
             <Link to={`/product/${product.Slug}`}>
               <img src={product.image} alt={product.name} />

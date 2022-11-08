088fba3d1cc4dedbcdfad837c92d8e6332575cb2
Aditya Pandya <pandyaaditya600@gmail.com>
2022-11-08 23:36:35 +0530

    Complete Cart Screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -54,7 +54,14 @@ function ProductScreen() {
   //About AddToCartHandler.
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { cart, userInfo } = state;
-  const addToCartHandler = () => {
+  const addToCartHandler = async () => {
+    const existItem = cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
     ctxDispatch({
       type: 'CART_ADD_ITEM',
       payload: { ...product, quantity: 1 },

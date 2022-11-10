e2e5b5fb5bbc2bc7168ec67275ad4811d432a858
Aditya Pandya <pandyaaditya600@gmail.com>
2022-11-10 23:33:04 +0530

    Create Sign-In Api

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 mongoose
@@ -15,11 +16,20 @@ mongoose
     console.log(err.message);
   });
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 
 /* to access variable inside we use back tick literal rather
 than using single quotes */
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {

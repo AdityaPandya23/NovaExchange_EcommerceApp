6eae181de252a79eb717c5cd963255cb1e84eb98
Aditya Pandya <pandyaaditya600@gmail.com>
2022-11-10 20:50:10 +0530

    Create Sample Products

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 dotenv.config();
 mongoose
@@ -13,27 +15,11 @@ mongoose
     console.log(err.message);
   });
 const app = express();
+app.use('/api/seed', seedRouter);
+
 /* to access variable inside we use back tick literal rather
 than using single quotes */
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-app.get('/api/products/slug/:slug', (req, res) => {
-  const product = data.products.find((x) => x.Slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not found' });
-  }
-});
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not found' });
-  }
-});
+app.use('/api/products', productRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {

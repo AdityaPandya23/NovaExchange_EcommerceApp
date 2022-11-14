55586a0f162c8c88e5c56e8940214ee3b446311a
Aditya Pandya <pandyaaditya600@gmail.com>
2022-11-14 22:13:53 +0530

    last one

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -49,7 +49,7 @@ function HomeScreen() {
   return (
     <div>
       <Helmet>
-        <title>Amazona</title>
+        <title>E-Commerce Application</title>
       </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
